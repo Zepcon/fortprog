279bb4b6113eb1dba05e908caed9149d3032e5ee
Zepcon <flavioschroeder@gmx.de>
2020-02-17 16:15:55 +0100

    Update pretty_printing.hs
    prettylist

--- pretty_printing.hs
+++ pretty_printing.hs
@@ -11,21 +11,12 @@ class Pretty a where
 instance Pretty Term where
  pretty (Var x) = x
  pretty (Comb x []) = x
- -- pretty (Comb "." (x:y)) = undefined
- pretty (Comb "." ((Comb f t):xs)) = undefined
- pretty (Comb f x) = f ++ "(" ++ (intercalate ", " (map pretty x)) ++ ")"
+ pretty (Comb "." ((Comb f t):xs)) = "[" ++ prettyList (Comb f t) xs ++ "]"
+  where
+   prettyList :: Term -> Term - String
 
- -- funktioniert bereits:
- -- *Main> pretty (Var "A")
- -- "A"
- -- *Main> pretty (Comb "true" [])
- -- "true"
- -- *Main> pretty (Comb "f" [Var "B", Var "_", Comb "true" []])
- -- "f(B, _, true)"
- -- *Main> pretty (Comb "." [Var "G"])
- -- ".(G)"
- -- *Main> pretty (Comb "." [Var "H", Comb "true" [], Comb "j" [Var "I"]])
- -- ".(H, true, j(I))"
+
+ pretty (Comb f x) = f ++ "(" ++ (intercalate ", " (map pretty x)) ++ ")"
 
  -- zeigt was Falsches an:
 -- *Main> pretty (Comb "." [Comb "true" [], Comb "[]" []])
