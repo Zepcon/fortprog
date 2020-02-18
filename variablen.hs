7aeb799c13d53e4f3d1931bb80685c74a7d154fb
lara6 <38834687+lara6@users.noreply.github.com>
2020-02-18 18:18:10 +0100

    Update variablen.hs
    Laras Idee für die unendliche Liste

--- variablen.hs
+++ variablen.hs
@@ -7,6 +7,7 @@ class Vars a where
 
 instance Vars Term where
  allVars (Var a) = [a]
+ allVars (Comb _ []) = []
  allVars (Comb _ x) = varHelp [] x
    where
     varHelp :: [VarName] -> [Term] -> [VarName]
@@ -15,35 +16,50 @@ instance Vars Term where
 
 instance Vars Rule where
  allVars (Rule (Var a) []) = [a]
+
+ --
+ -- warning: [-Wincomplete-patterns]
+ --     Pattern match(es) are non-exhaustive
+ --     In an equation for `allVars':
+ --         Patterns not matched: (Rule (Var _) (_:_))
+
  allVars (Rule (Comb _ []) []) = []
  allVars (Rule (Comb _ x) y) = varHelp [] x y
   where
    varHelp :: [VarName] -> [Term] -> [Term] -> [VarName]
    varHelp acc [] [] = acc
-   varHelp acc x [] = acc ++ (concat (map allVars x))
-   varHelp acc [] x = acc ++ (concat (map allVars x))
-   varHelp acc x y = acc ++ (concat (map allVars x)) ++ (concat (map allVars y))
+   varHelp acc x1 [] = acc ++ (concat (map allVars x1))
+   varHelp acc [] x1 = acc ++ (concat (map allVars x1))
+   varHelp acc x1 y1 = acc ++ (concat (map allVars x1)) ++ (concat (map allVars y1))
 
 
-instance Vars Prog where
- allVars Prog [] = []
- allVars Prog  x = varHelp [] x
-  where
-   varHelp :: [VarName] -> [Term] -> [VarName]
-   varHelp acc [] = acc
-   varHelp acc [Rule (Var a) []] = acc ++ [a]
-   
+-- instance Vars Prog where
+--  allVars Prog [] = []
+--  allVars Prog  x = varHelp [] x
+--   where
+--    varHelp :: [VarName] -> [Term] -> [VarName]
+--    varHelp acc [] = acc
+--    varHelp acc [Rule (Var a) []] = acc ++ [a]
+
 
 instance Vars Goal where
- allVars = undefined
+  allVars (Goal []) = []
+  --
+  -- Goal (Var _:_:_))
+  --             (Goal (Comb _ []:_:_))
+  --             (Goal (Comb _ (_:_):_:_))
+  --    |
+
+  allVars (Goal [Var(a)]) = [a]
+  allVars (Goal [Comb _ []]) = []
+  allVars (Goal [Comb _ x]) = varHelp [] x --Hilfsfunktion
+    where
+      varHelp :: [VarName] -> [Term] -> [VarName]
+      varHelp acc [] = acc
+      varHelp acc y = acc ++ (concat (map allVars y))
+
 
 
  -- unendliche Liste
 freshVars :: [VarName]
-freshVars = undefined
-
-{-
-- Variablen beginnen mit einem Großbuchstaben
-- Variablen stehen für beliebige andere Objekte
-- Regeln oder Fakten mit Variablen repräsentieren unendlich viele Regeln
- -}
+--freshVars = [ c | c `elem` ['A'..'Z'], c ++ (show x1) | x1 <- [1..]::[Integer]]  
