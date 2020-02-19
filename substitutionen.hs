480cc9e1e3b261592a966d1bb91e669146d90094
Zepcon <flavioschroeder@gmx.de>
2020-02-19 18:42:26 +0100

    apply lüppt

--- substitutionen.hs
+++ substitutionen.hs
@@ -5,12 +5,12 @@ module Subs where
 import Type
 
 -- Substitutionen, zugeordnet zu Variablen
-data Subst = Empty | Subst [(VarName,Term)]
+data Subst = Subst [(VarName,Term)]
  deriving Show
 
 -- empty Substitution
 empty :: Subst
-empty = Empty
+empty = Subst []
 
 -- 1 Variable auf 1 Term
 single :: VarName -> Term -> Subst
@@ -18,7 +18,18 @@ single x y = Subst [(x,y)]
 
 -- Substitution auf Term anwenden
 apply :: Subst -> Term -> Term
-apply s [] = []
-apply s [Var(a)] = undefined --Subsitution nur auf die Variable anwenden
-apply s (Comb _ []) = []
-apply s (Comb _ x) = undefined --Subsitution nur auf die Variable anwenden
+apply (Subst []) a = a
+apply (Subst ((x,y):z)) (Var a) = if x == a then y else apply (Subst z) (Var a)
+apply s (Comb a x) = Comb a (map (apply s) x)
+--apply (Subst ((x,y):z)) (Comb _ a) = undefined
+
+
+-- Subst example:
+-- Subst [("A",Var "B"), ("C", Var "D")]
+-- Subst [("A",Var "B"), ("C", Comb "." [Comb "true" [], Comb "." [Comb "g" [Var "C"], Comb "[]" []]])]
+
+-- Term example:
+-- Var "A"
+-- Comb "." [Var "B"]
+-- (Comb "." [Var "K", Comb "." [Var "L", Var "M", Var "N", Var "O"]])
+-- (Comb "." [Var "E", Comb "h" [Var "F", Comb "i" [Var "G"]]])
