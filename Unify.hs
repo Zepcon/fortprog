9f45a79636bfdf8ac8af9e0d0fc5cd05edb2eee3
lara6 <38834687+lara6@users.noreply.github.com>
2020-02-20 14:34:40 +0100

    Update Unify.hs

--- Unify.hs
+++ Unify.hs
@@ -3,11 +3,18 @@ module Unify where
 import Type
 import Subs
 
+-- ds berechnet die Unstimmigkeitsmenge zweier Terme
+-- und gibt sie als Paar zurück
+-- Unstimmigkeitsmenge leer: Nothing zurückgeben
+-- Umgang mit anonymen Variablen (_)
+
 ds :: Term -> Term -> Maybe (Term, Term)
 
 
 
 
 
-
+-- unify bestimmt aufbauend auf ds den allgemeinsten Unifikator für zwei
+-- sofern die beiden Terme unifizierbar sind
+-- sonst: Nothing
 unify :: Term -> Term -> Maybe Subst
