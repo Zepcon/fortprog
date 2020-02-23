d8c618a03ec413d6d011cf07a34ca7f78cf0142b
Zepcon <flavioschroeder@gmx.de>
2020-02-23 23:47:45 +0100

    Rename mit dummen bug

--- Rename.hs
+++ Rename.hs
@@ -1,7 +1,20 @@
 module Rename where
 
 import Type
+import Vars
 
--- Variablen einer Regel umbenennen
-rename :: Rule -> VarName -> Rule
-rename = undefined
+-- Variante einer Regel = Regel mit neuen Variablen
+-- Variablen einer Regel umbenennen, Rule = Rule Term [Term]
+rename :: Rule -> Rule
+rename (Rule (Var "_") []) = Rule (Var "_") []
+rename (Rule (Var x) []) = Rule (Var (head (take 1 (filter (\y -> (not (y == x))) freshVars)))) []  -- keine Klammern wegnehmen!
+rename (Rule (Var x) y) = Rule (renameTerm (Var x)) (map renameTerm y)
+rename (Rule (Comb a x) y) = Rule (Comb a (map renameTerm x)) (map renameTerm y)
+
+renameTerm :: Term -> Term
+renameTerm (Var x) = Var (head (take 1 (filter (\y -> (not (y == x))) freshVars)))
+renameTerm (Comb a x) = Comb a (map renameTerm x)
+
+
+r1 = Rule (Var "A") [Var "B", Comb "." [Var "C"]]
+r2 = Rule (Var "A") []
