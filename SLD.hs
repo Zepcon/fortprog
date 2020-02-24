71f4437c27f1b44df062477ba6afca7bc37c93d1
Zepcon <flavioschroeder@gmx.de>
2020-02-24 17:13:08 +0100

    keine Lust mehr

--- SLD.hs
+++ SLD.hs
@@ -12,20 +12,28 @@ sld :: Prog -> Goal -> SLDTree
 sld (Prog []) (Goal []) = SLDTree (Goal []) []
 sld (Prog _) (Goal []) = SLDTree (Goal []) []
 sld (Prog []) (Goal x) = SLDTree (Goal x) []
-sld (Prog rs) (Goal g) = undefined --let a = (map rename rs) in map
+sld (Prog rs) (Goal g) =let rs' = map rename rs
+                            mps = map (\r -> helper r g) rs'
+                            fmps = filter (/= Nothing) mps
+                            fps = helper2 fmps
+                        in 
 
-     let rs' = map rename rs
-         mps = map (\r -> helper r g) rs'
-         fmps = filter (/= Nothing) mps
 
-
-         
+helper2 :: [Maybe (Subst,Goal)] -> [(Subst, Goal)]
+helper2 [] = []
+helper2 [(Just (x,y))] = [(x,y)]
+helper2 (Just ((x,y):xs)) = helper2ac [] (Just (x,y):xs)
+helper2 _ = error "What just happened."
+ where
+  helper2ac :: [(Subst, Goal)] -> [Maybe (Subst, Goal)] -> [(Subst, Goal)]
+  helper2ac acc [] = acc
+  helper2ac acc (Just (x,y):xs) = acc ++ [(x,y)] ++ (helper2 xs)
+  
 
 helper :: Rule -> Goal -> Maybe (Subst,Goal)
 helper (Rule t ts) (Goal (g:gs) = case unify t g of
                                     Nothing -> Nothing
                                     Just s  -> (s, Goal (map (apply s) (ts ++ gs)))
-
 
 
 -- Regelkopf mit Literal unifizieren
