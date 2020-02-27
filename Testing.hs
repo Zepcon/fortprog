ba1257a3ce5b635479173a9efea2783d44a02793
lara6 <38834687+lara6@users.noreply.github.com>
2020-02-27 14:19:56 +0100

    Update Testing.hs

--- Testing.hs
+++ Testing.hs
@@ -11,6 +11,9 @@ sldt = sld pro go
 pro :: Prog
 pro = Prog [rule1, rule2]
 
+-- append([],L,L).
+-- append([E|R],L,[E|RL]) :- append(R,L,RL).
+
 rule1:: Rule
 rule1 = Rule (Comb "append" [Comb "[]" [], Var "L", Var "L"]) []
 
@@ -25,17 +28,33 @@ go = Goal [Comb "append" [Var "X", Var "Y", Comb "." [Comb "1" [], Comb "." [Com
 
 
 ------------------------------------------------------------------------------
-
-sldt2 :: SLDTree
-sldt2 = sld pro2 goal
-
-pro2 :: Prog
-pro2 = Prog [rule1a, rule2a]
-
-rule1a:: Rule
-
-rule 2a :: Rule
-
--- =(f(A,B),f(f(C),g(A))).
-
-goal = Goal [Comb "." [Comb "f" [Var "A", Var "B"], Comb "f" [Comb "f" [Var "C"]], Comb "g" [Var"A"]]]
+-- sldt2 :: SLDTree
+-- sldt2 = sld pro2 go2
+--
+-- pro2 :: Prog
+-- pro2 = Prog [r1, r2, r3]
+--
+-- -- ehemann(christine, heinz).
+-- -- ehemann(maria, fritz).
+-- -- ehemann(monika, herbert).
+-- -- ehemann(angelika, hubert).
+-- r1 :: Rule
+-- r1 = Rule (Comb "ehemann" [Var "christine" Var "heinz"])
+-- r2 = Rule (Comb "ehemann" [Var "maria" Var "fritz"])
+-- r3 = Rule (Comb "ehemann" [Var "monika" Var "herbert"])
+-- r4 = Rule (Comb "ehemann" [Var "angelika" Var "hubert"])
+--
+-- -- mutter(herbert, christine).
+-- -- mutter(angelika, christine).
+-- -- mutter(hubert, maria).
+-- -- mutter(susanne, monika).
+-- -- mutter(norbert, monika).
+-- -- mutter(andreas, angelika).
+-- --
+-- -- vater(K, V) :- ehemann(M, V), mutter(K, M).
+-- --
+-- -- elter(K, E) :- vater(K, E).
+-- -- elter(K, E) :- mutter(K, E).
+-- --
+-- -- vorfahre(N, V) :- vorfahre(N, V2), vorfahre(V2, V).
+-- -- vorfahre(N, V) :- elter(N, V).
